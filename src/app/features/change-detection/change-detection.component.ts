25b8c774fde0f5269ca5b8b3bf665eb5332ad750
Martin Pacuch <pacuch.martin@gmail.com>
2025-05-19 22:17:22 +0200

    detection on push 2

---
+++ src/app/features/change-detection/change-detection.component.ts
@@ -0,0 +1,44 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { DetectionListComponent } from './list/detection-list.component';
+import { MatButton } from '@angular/material/button';
+import { Subject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionService } from './change-detection.service';
+
+@Component({
+  selector: 'app-change-detection',
+  imports: [DetectionListComponent, MatButton, AsyncPipe],
+  providers: [ChangeDetectionService],
+  templateUrl: './change-detection.component.html',
+  styleUrl: './change-detection.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChangeDetectionComponent implements OnInit {
+  private changeDetectionService = inject(ChangeDetectionService);
+  randomGreeting$ = new Subject<string>();
+  randomGreeting = '';
+  randomGreetingService = '';
+
+  ngOnInit() {
+    this.randomGreeting$.subscribe((greeting) => {
+      this.randomGreeting = greeting;
+    });
+
+    this.changeDetectionService.randomGreeting$.subscribe((greeting) => {
+      this.randomGreetingService = greeting;
+    });
+  }
+
+  updateLocalSubject() {
+    this.randomGreeting$.next(`Hello User ${Math.floor(Math.random() * 100)}`);
+  }
+
+  updateServiceSubject() {
+    this.changeDetectionService.updateSubject();
+  }
+}
---
+++ src/app/features/change-detection/list/detection-list.component.ts
@@ -0,0 +1,35 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+} from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatList, MatListItem } from '@angular/material/list';
+
+@Component({
+  selector: 'app-detection-list',
+  imports: [MatButton, MatList, MatListItem],
+  templateUrl: './detection-list.component.html',
+  styleUrl: './detection-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DetectionListComponent {
+  private cdr = inject(ChangeDetectorRef);
+
+  itemList = [
+    { name: 'Awesome Item 1' },
+    { name: 'Awesome Item 2' },
+    { name: 'Awesome Item 3' },
+  ];
+
+  ngOnInit() {}
+
+  pushItemToItemList() {
+    this.itemList.push({ name: 'Awesome Item 4' });
+    setTimeout(() => {
+      this.itemList.push({ name: 'Awesome Item 5' });
+      this.cdr.markForCheck();
+    }, 1000);
+  }
+}
